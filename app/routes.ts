162b438bc7deb5fdae795d877a9a8c57f18dd710
Aohk22 <96096597+Aohk22@users.noreply.github.com>
2026-03-01 17:06:32 +0700

    init

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,21 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+	type RouteConfig,
+	layout,
+	route
+} from "@react-router/dev/routes"
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+	route('/login', './routes/Login.tsx'),
+
+	route('/', './routes/Protected.tsx', [
+		layout('./routes/layout.tsx', [
+			route('/', './routes/dashboard/Dashboard.tsx'),
+
+			route('/courses', './routes/course/Courses.tsx'),
+			route('/courses/:courseId', './routes/course/CourseDetail.tsx'),
+			route('/courses/:courseId/lessons/:lessonId', './routes/course/Lesson.tsx'),
+
+			route('*', 'routes/CatchAll.tsx')
+		]),
+	]),
+] satisfies RouteConfig
